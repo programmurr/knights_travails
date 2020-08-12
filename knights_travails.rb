4218148fb7553fc2420b7d16af11b7fce28a9fc8
programmurr <conorjm89@gmail.com>
2020-08-12 23:34:06 +0100

    Board, Cell and Knight classes updated

--- knights_travails.rb
+++ knights_travails.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-require 'pry'
-
 class Cell
   attr_accessor :value, :co_ords
 
@@ -15,15 +13,7 @@ class Knight
   attr_accessor :piece, :position
   def initialize
     @piece = 'Kn'
-    @position = nil
-  end
-
-  def get_knight_position(grid)
-    grid.each do |row|
-      row.each do |cell|
-        return @position = cell.co_ords if cell.value
-      end
-    end
+    @position = []
   end
 end
 
@@ -32,20 +22,21 @@ class Board
 
   def initialize(input = {})
     @grid = input.fetch(:grid, default_grid)
+    set_cell_coordinates
   end
 
   def get_cell(x, y)
     grid[x][y]
   end
 
-  def set_cell(x, y, value)
-    get_cell(x, y).value = value
-    grid[x][y].co_ords = [x, y]
+  def set_cell(x, y, piece)
+    get_cell(x, y).value = piece
+    piece.position = [x, y]
   end
 
   def formatted_grid
     grid.each do |row|
-      puts row.map { |cell| cell.value ? '_' : cell.value }.join(' ')
+      puts row.map { |cell| cell.value == '' ? '_' : cell.value.piece.to_s }.join(' ')
     end
   end
 
@@ -54,12 +45,27 @@ class Board
   def default_grid
     Array.new(8) { Array.new(8) { Cell.new } }
   end
+
+  def set_cell_coordinates
+    x = 0
+    while x < 8
+      y = 0
+      while y < 8
+        grid[x][y].co_ords = [x, y]
+        y += 1
+      end
+      x += 1
+    end
+  end
 end
 
 board = Board.new
-knight = Knight.new
-board.set_cell(7, 1, knight)
+white_knight = Knight.new
+black_knight = Knight.new
+board.set_cell(7, 1, white_knight)
+board.set_cell(0, 6, black_knight)
 board.formatted_grid
-knight.get_knight_position(board.grid)
 p board.get_cell(7, 1)
-p knight
+p board.get_cell(0, 6)
+p white_knight
+p black_knight
