d92c7b9d912bac0a64cc317d14de009d1276d034
programmurr <conorjm89@gmail.com>
2020-08-13 20:30:42 +0100

    Have a working graph now. Comments describe what to do next

--- graphs.rb
+++ graphs.rb
@@ -1,14 +1,60 @@
 # frozen_string_literal: true
 
-class MoveGraph
-  def initialize(move_array)
-    build_graph(move_array)
+require 'pry'
+require_relative 'chess'
+
+class Node
+  attr_reader :name
+  def initialize(name, co_ord)
+    @name = name
+    @co_ord = co_ord
+    @successors = []
   end
 
-  def build_graph(array)
-    # Make a graph of all current mossible moves
-  end
+  def add_edge(successor)
+    @successors << successor
   end
 
-# Make a Tree, or graph, that can display all of the knight's current moves?
-# Make an adjascency list for how knight can get from one place to another, then pick the shortest list?
+  def to_s
+    "#{@name} -> [#{@successors.map(&:name).join(' ')}]"
+  end
+end
+
+class Graph
+  def initialize
+    @nodes = {}
+  end
+
+  def add_node(node)
+    @nodes[node.name] = node
+  end
+
+  def add_edge(predecessor_name, successor_name)
+    @nodes[predecessor_name].add_edge(@nodes[successor_name])
+  end
+
+  def [](name)
+    @nodes[name]
+  end
+end
+
+graph = Graph.new
+graph.add_node(Node.new(:current_pos, [7, 1]))
+graph.add_node(Node.new(:move1, [5, 0]))
+graph.add_node(Node.new(:move2, [5, 2]))
+graph.add_node(Node.new(:move3, [6, 3]))
+graph.add_edge(:current_pos, :move1)
+graph.add_edge(:current_pos, :move2)
+graph.add_edge(:current_pos, :move3)
+p graph[:current_pos]
+
+# Make a program that:
+# Gets knight's current position
+# Turns it into a node
+# Calculates the next possible positions
+# Turns them into nodes
+# Adds all those nodes to the graph
+# Traverses from one node to another when told
+# Re-calculates next possible positions
+# Can traverse to one of them
+# And so on...
--- main.rb
+++ main.rb
@@ -8,6 +8,4 @@ board = Board.new
 white_knight = Knight.new
 board.set_cell(7, 1, white_knight)
 board.formatted_grid
-possible_moves = white_knight.possible_moves(white_knight.position)
-knight_tree = MoveGraph.new(possible_moves)
-p knight_tree
+p possible_moves = white_knight.possible_moves(white_knight.position)
